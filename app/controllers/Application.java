ebca8bf79f2b6734f3de0448ed4ddd26dc51f9bd
Neldin Dzekovic <neldin.dzeko@gmail.com>
2015-02-26 10:36:42 +0100

    Added registration() method in controller.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,32 @@
 package controllers;
 
+import models.User;
 import play.*;
+import play.data.Form;
 import play.mvc.*;
-
 import views.html.*;
 
 public class Application extends Controller {
 
     public static Result index() {
         return ok(index.render("Your new application is ready."));
+    }
+    
+    static Form<User> newUser = new Form<User>(User.class);
+    
+    public static Result registration() {
+    	String email = newUser.bindFromRequest().get().email;
+    	String password = newUser.bindFromRequest().get().hashedPassword;
+    	boolean isSuccess = User.createUser(email, password);
+    	if (isSuccess == true) {
+    		return ok(index.render("Registration success."));
+    	} else {
+    		return ok(index.render("Registration failed."));
+    	}
+    }
+    
+    public static Result login() {
+    	return TODO;
     }
 
 }
---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,28 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table user (
+  id                        integer not null,
+  email                     varchar(255),
+  hashed_password           varchar(255),
+  constraint uq_user_email unique (email),
+  constraint pk_user primary key (id))
+;
+
+create sequence user_seq;
+
+
+
+
+# --- !Downs
+
+SET REFERENTIAL_INTEGRITY FALSE;
+
+drop table if exists user;
+
+SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists user_seq;
+
