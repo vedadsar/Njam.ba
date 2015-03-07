0d88e0e64193f54179ec5e767f86d9a681265820
GorjanGOA <kalauzovic.gorjan@gmail.com>
2015-03-08 00:24:34 +0100

    Gorjan modify Meal

--- app/models/Meal.java
+++ app/models/Meal.java
@@ -18,33 +18,33 @@ public class Meal extends Model {
 	public String name;
 	@Required
 	public double price;
-	
+
 	@OneToOne
 	public Restaurant restaurant;
-	
-	static Finder<Integer, Meal> find =  new Finder<Integer, Meal>(Integer.class, Meal.class);
 
-	
-	public Meal(String name, double price){
-		this.name=name;
-		this.price =price;
+	static Finder<Integer, Meal> find = new Finder<Integer, Meal>(
+			Integer.class, Meal.class);
+
+	public Meal(String name, double price) {
+		this.name = name;
+		this.price = price;
 	}
-	
-	public Meal(String name, double price, Restaurant restaurant){
-		this.name=name;
-		this.price =price;
+
+	public Meal(String name, double price, Restaurant restaurant) {
+		this.name = name;
+		this.price = price;
 		this.restaurant = restaurant;
 	}
-	
-	
-	public static boolean create(String name, double price){		
-			Meal m = new Meal(name, price);
-			User u =  Session.getCurrentUser(Context.current());
-			m.restaurant = u.restaurant;
-			m.save();		
-			return true;
+
+	public static boolean create(String name, double price) {
+		Meal m = new Meal(name, price);
+		User u = Session.getCurrentUser(Context.current());
+		m.restaurant = u.restaurant;
+		m.save();
+
+		return true;
 	}
-	
+
 	public static boolean create(Meal m) {
 		Meal meal = find.findUnique();
 		if (meal != null) {
@@ -54,24 +54,37 @@ public class Meal extends Model {
 		}
 		return true;
 	}
-	
-	public static Meal find(int id){
-		return find.byId(id);
+
+	public static void delete(int id) {
+		find.byId(id).delete();
 	}
-	
-	public static Meal findByName(String name){
-		return find.where().eq("name", name).findUnique();
+
+	public static List<Meal> all(String name) {
+		return find.where().eq("name", name).findList();
 	}
-	
-	public static void delete(int id){
-		 find.byId(id).delete();
-	}
-	
-	public static List<Meal> all(){
+
+	public static List<Meal> all() {
 		return find.all();
 	}
-	
-	public static List<Meal> all(String name){
-		return find.where().eq("name", name).findList();
+
+	public static Meal find(int id) {
+		return find.byId(id);
+	}
+
+	public static Meal findByName(String name) {
+		return find.where().eq("name", name).findUnique();
+	}
+
+	public static boolean modifyMeal(Meal m, String newName, double newPrice) {
+
+		m.name = newName;
+		m.price = newPrice;
+		m.update();
+		Meal meal = find.findUnique();
+		if (!meal.name.equals(newName) || meal.price != newPrice) {
+			return false;
+		}
+		else
+			return true;
 	}
 }
