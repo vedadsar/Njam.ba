02105cf33ff585fc48d9fa2809255e44f0d3449f
Vedad <vedad.zornic@bitcamp.ba>
2015-03-07 01:10:39 +0100

    Fixed small bug with restaurant redirection

--- app/controllers/RestaurantController.java
+++ app/controllers/RestaurantController.java
@@ -39,7 +39,7 @@ public class RestaurantController extends Controller {
 		Double price = Double.parseDouble(mealPrice);
 		
 		if (Meal.create(mealName, price) == true) {
-			return ok(user.render(Session.getCurrentUser(ctx()).email));
+			return ok(restaurant.render(Session.getCurrentUser(ctx()).email));
 		}
 		return TODO;
 	}
