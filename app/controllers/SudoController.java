7a1af230f94d922ca57a1986f445ad3a9177c224
vedadsar <vedadsa@windowslive.com>
2015-03-18 21:28:07 +0100

    Added logs in sudo controller

--- app/controllers/SudoController.java
+++ app/controllers/SudoController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import models.*;
+import play.Logger;
 import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -8,8 +9,11 @@ import play.mvc.Security;
 import views.html.*;
 import play.db.ebean.Model.Finder;
 import Utilites.*;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class SudoController extends Controller{
 
@@ -63,6 +67,52 @@ public class SudoController extends Controller{
 		User u = User.find(email);
 
 		return ok(admin.render(email,meals, restaurants));
+	}
+	
+	@Security.Authenticated(AdminFilter.class)
+	public static Result logs(){
+		List logs = logList();
+		return TODO;
+	}
+	
+	/**
+	 * Method reads application.log file which contains all the logs.
+	 * It return all logs as list of strings.
+	 * @return list of logs as strings.
+	 */
+	public static List<String> logList(){
+		File f = new File("./logs/application.log");
+		List<String> listOfLogs = new ArrayList<String>();
+		Scanner sc = null;
+		try {
+			sc = new Scanner(f);
+			while(sc.hasNextLine()){
+				String line = sc.nextLine();
+				line.trim();
+				
+				if(line.isEmpty())
+					continue;
+				if(line.charAt(0) != '2' || line.length() < 1){
+					continue;
+				}
+				String log = "";
+				if(line.charAt(0) == '2'){
+					log = line;
+					line = sc.nextLine();
+					if(!line.isEmpty()){
+						log += "\n" +line ;
+					}
+					log += "\n";
+				}
+				listOfLogs.add(log);
+			}
+			
+		} catch (FileNotFoundException e) {
+			Logger.error("Could not list logs");
+		}finally{
+			sc.close();
+		}	
+		return listOfLogs;
 	}
 
 }
