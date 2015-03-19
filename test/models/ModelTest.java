1a32b52929fdd5fb42aefdcd2e4ae5205d35efc0
vedadsar <vedadsa@windowslive.com>
2015-03-20 00:05:19 +0100

    Created model tests

--- test/models/ModelTest.java
+++ test/models/ModelTest.java
@@ -1,15 +1,13 @@
 package models;
 
-import java.util.List;
-
-import org.junit.*;
-
-import Utilites.Hash;
-import play.test.WithApplication;
 import static org.junit.Assert.*;
 import static play.test.Helpers.*;
+import models.User;
+import org.junit.*;
+import Utilites.Hash;
+import play.test.WithApplication;
 
-public class ModelTest extends WithApplication {
+public class ModelTest extends WithApplication  {
 
 	@Before
 	public void setUp(){
@@ -17,47 +15,63 @@ public class ModelTest extends WithApplication {
 	}
 	
 	@Test
-	public void testCreate(){
-		User.createUser("test@email.com", "123456");
-		User u = User.find(5);
-		assertNotNull(u);
-		assertEquals(u.email,"test@email.com");
-		assertTrue( Hash.checkPassword("123456", Hash.hashPassword("123456")));
-	}
-	
-	@Test
-	public void testFindNonExisting(){
-		User u = User.find(100);
-//		assertNotNull(u);
-		assertNull(u);
-	}
-	
-	@Test
-	public void testDelete(){
-		User.createUser("test@email.com", "test");
-		User.delete(1);
-		User u = User.find(1);
-		assertNull(u);
+	public void restaurant(){
+		//creating userRestaurant model
+		User.createRestaurant("Test1", "Test@test.ba", "test", "test", "Test", "test");
+		User u = User.find(5);		
+		assertNotNull(u);		
+		assertEquals(User.RESTAURANT, u.role);
+		assertEquals(u.email, "Test@test.ba");
+		assertTrue( Hash.checkPassword("test",u.hashedPassword));	
+		//Creating restaurant model
+		Restaurant.create("Test", null);
+		Restaurant r = Restaurant.find(4);
+		assertNotNull(r);		
 		
 	}
-//	
-//	
-//	@Test
-//	public void testAll(){
-//		User[] all = {
-//				new User("test1@email.com", "test"),
-//				new User("test2@email.com", "test"),
-//				new User("test3@email.com", "nijeTest"),
-//				new User("test4@email.com", "test")
-//		};
-//		
-//		for ( User u : all){
-//			u.save();
-//		}
-//		
-//		assertEquals(2, User.allUsers().size());
-//		List<User> base =  User.allUsers();
-//		assertTrue(base.contains(all[0]));
-//		assertFalse(base.contains(all[2]));
-//	}
+	
+	@Test
+	public void user(){
+		User.createUser("Test@test.ba", "test");
+		User u = User.find(5);		
+		assertNotNull(u);	
+		assertEquals(User.USER, u.role);
+		assertEquals(u.email, "Test@test.ba");
+		assertTrue( Hash.checkPassword("test",u.hashedPassword));
+		
+		User.deleteUser(5);
+		assertNull(User.find(5));
+	}
+
+	
+	@Test
+	public void mealTest(){
+		//Testing meal creation
+		Meal.create("Test", 5, null);
+		Meal m = Meal.find(1);
+		assertNotNull(m);
+		assertEquals("Test", m.name);
+		
+		//Testing meal update
+		Meal.modifyMeal(m, "Test1", 2);
+		assertEquals("Test1", m.name);
+		
+		//Testing delete meal
+		int id = m.id;
+		Meal.delete(id);
+		assertNull(Meal.find(id));
+	}
+	
+	@Test
+	public void faqTest(){
+		Faq.create("Test question", "Test answer");
+		Faq f = Faq.find(1);
+		assertNotNull(f);
+		assertEquals(f.answer, "Test answer");
+		
+		int id = f.id;
+		Faq.delete(id);
+		assertNull(Faq.find(id));
+	}
+	
 }
