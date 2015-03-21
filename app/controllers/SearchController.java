fca25372a638442e93e00128ae1c2c83c4947e0d
GorjanGOA <kalauzovic.gorjan@gmail.com>
2015-03-21 17:20:41 +0100

    Pretraga restorana i meala + controlleri za search + Main search

--- app/controllers/SearchController.java
+++ app/controllers/SearchController.java
@@ -4,7 +4,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 import java.util.UUID;
-
+import play.Logger;
 import models.Faq;
 import models.Location;
 import models.Meal;
@@ -21,15 +21,65 @@ import play.data.DynamicForm;
 import play.db.ebean.Model.Finder;
 import Utilites.*;
 
-
 public class SearchController extends Controller {
-	
-	public static Result autocomplete(String q){
-		List<Meal> meals = Meal.find.where().like("name", "%" + q + "%").findList();
-				
-		for(Meal m : meals){
+
+	public static DynamicForm form = DynamicForm.form().bindFromRequest();
+
+	public static Result autocomplete(String q) {
+		List<Meal> meals = Meal.find.where().like("name", "%" + q + "%")
+				.findList();
+
+		for (Meal m : meals) {
 			return TODO;
 		}
 		return TODO;
 	}
+
+	
+	
+	public static Result searchByGeneric(String q,String typeGen) {
+		List <Restaurant> RestaurantEmpty=null;
+		List<Meal> mealsEmpty = null;
+		String currentEmail=null;
+	//	String typeGen= form.data().get("typeGen");
+		Logger.debug(typeGen);
+		
+	
+	//Check if the current user is logged in;
+	 if (Session.getCurrentUser(ctx())!=null)
+     {currentEmail =Session.getCurrentUser(ctx()).email ;
+     
+     }
+	 
+	 //check to se  what tipe value  we  are searching
+	    if (typeGen==null){
+	    	return  ok(searchResult.render(" ",currentEmail,Restaurant.all(),RestaurantEmpty,searchAllMeals(q)));
+	    }
+	 
+		if (typeGen.equals("Meal")) {
+			return ok(searchResult.render(" ",currentEmail,Restaurant.all(),RestaurantEmpty, searchAllMeals(q)
+					));
+			
+			
+		}  if (typeGen.equals("Restaurant")) {
+			return ok(searchResult.render(" ",currentEmail,Restaurant.all(),searchAllRestaurants(q),mealsEmpty));
+		
+		}
+		return TODO;
+	}
+
+	
+	public static List<Meal> searchAllMeals(String q) {
+		List<Meal> meals                   = Meal.find.where().ilike("name", "%" + q+ "%")
+				.findList();
+			
+		return meals;
+	}
+	
+
+	public static List searchAllRestaurants(String q) {
+		List<Restaurant> restaurants = Restaurant.find.where().ilike("name", "%" + q + "%").findList();
+		return restaurants;
+	}
+
 }
