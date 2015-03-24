f29f9f64f9d0ba14f11c5597e7e788a09fed016e
Davor Stanković <dastko@gmail.com>
2015-03-24 22:31:56 +0100

    Added Cart Controller & new routes

--- app/controllers/CartController.java
+++ app/controllers/CartController.java
@@ -2,11 +2,14 @@ package controllers;
 
 import play.mvc.Result;
 import views.html.*;
+
 import java.util.Iterator;
 import java.util.List;
+
 import Utilites.AdminFilter;
 import Utilites.Session;
 import models.*;
+import models.orders.Cart;
 import play.Logger;
 import play.data.DynamicForm;
 import play.data.Form;
@@ -18,13 +21,27 @@ import Utilites.*;
 
 public class CartController extends Controller {
 	
+	static String email;
+	
 	public static Result showCart(){
-		return ok(cart.render("Nesto"));
+		email = session("email");
+		return ok(cart.render(email));
 	}
 	
 	public static Result addMealToBasket(int id){
 		Meal meal = Meal.find(id);
-		return null;
+//		User user = User.find(id);
+//		Cart cart = new Cart(user.id);
+//		cart.addMeal(meal);
+//		cart.save();
+		flash("SucessAdded", "Meal...");
+		return redirect("/");
 	}
-
+	
+	public static Result viewMeal(int id){
+		Meal meal = Meal.find(id);
+		email = session("email");
+		return ok(mealView.render(email, meal));
+	}
+	
 }
