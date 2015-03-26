7933834fd01ebfbc26e818c3a4da1a5e3bc82fe7
Uqluiorrae <hariskrkalic@gmail.com>
2015-03-26 17:53:37 +0100

    added mealView test

--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -1,12 +1,14 @@
+import models.Meal;
+import models.Restaurant;
+import models.User;
+
 import org.junit.*;
 
 import play.mvc.*;
 import play.test.*;
 import play.libs.F.*;
-
 import static play.test.Helpers.*;
 import static org.fest.assertions.Assertions.*;
-
 import static org.fluentlenium.core.filter.FilterConstructor.*;
 
 public class IntegrationTest {
@@ -69,5 +71,23 @@ public class IntegrationTest {
 //				});
 //
 //	}
+	
+	@Test
+	public void mealViewTest() {
+		running(testServer(3333, fakeApplication(inMemoryDatabase())),
+				HTMLUNIT, new Callback<TestBrowser>() {
+					public void invoke(TestBrowser browser) {
+						
+						User test = new User("test@test.ba", "test");
+						Restaurant urbanGrill = new Restaurant("haris", test);
+						Meal.create("Sarma", 15, urbanGrill );
+						
+						browser.goTo("http://localhost:3333/");
+						assertThat(browser.pageSource()).contains("Sarma");
+						assertThat(browser.pageSource()).contains("Food price");
+
+					}
+				});
+	}
 	
 }
