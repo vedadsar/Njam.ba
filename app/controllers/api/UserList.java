60d00c98cd2749fbd04143c4a9db2b8ef3814bf8
Davor Stanković <dastko@gmail.com>
2015-04-14 01:31:09 +0200

    Added ServiceAuth and confirm password value

--- app/controllers/api/UserList.java
+++ app/controllers/api/UserList.java
@@ -1,4 +1,4 @@
-package controllers.json;
+package controllers.api;
 
 import java.util.List;
 
@@ -12,16 +12,27 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 public class UserList {
 
 	public static ArrayNode usersList(List <User> users) {
-		ArrayNode array = new ArrayNode(JsonNodeFactory.instance);
+		ArrayNode arrayUsers = new ArrayNode(JsonNodeFactory.instance);
 
 		for (User u : users) {
 			ObjectNode user = Json.newObject();
+			user.put("id", u.id);
 			user.put("email", u.email);
-			user.put("role", u.role);
 			user.put("validated", u.validated);
-			array.add(user);
+			arrayUsers.add(user);
 		}
-		return array;
+		return arrayUsers;
 	}
+	
+	public static ObjectNode userToApp(User u) {
+		
+		ObjectNode user = Json.newObject();
+		user.put("id", u.id);
+		user.put("name", u.email);
+		user.put("email", u.validated);
+
+		return user;
+	}
+
 
 }
