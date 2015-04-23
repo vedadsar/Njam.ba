337ba1937b54f34de166e5d0206474e8fe4fa668
Davor Stanković <dastko@gmail.com>
2015-04-24 01:50:52 +0200

    Added newsletter unsubscribe, new Newsletter/Validate controllers and routes

--- app/controllers/Validate.java
+++ app/controllers/Validate.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import models.Newsletter;
 import models.User;
 import play.Logger;
 import play.data.Form;
@@ -11,22 +12,54 @@ import play.mvc.Security;
 import views.*;
 
 public class Validate extends Controller {
-	
-	public static Result validateEmail(String confirmationString) {
-		
-        User user = User.findByConfirmationString(confirmationString);
 
-        if (confirmationString == null) {
-            flash("error", Messages.get("error"));
-            return redirect("/");
-        }
-        
-        if (User.confirm(user)) {
-            flash("success", Messages.get("Successfully validated"));
-            return redirect("/login");
-        } else {
-            flash("errorLink", Messages.get("error.confirm"));
-            return redirect("/");
-        }
-    }
+	public static Result validateEmail(String confirmationString) {
+
+		User user = User.findByConfirmationString(confirmationString);
+
+		if (confirmationString == null) {
+			flash("error", Messages.get("error"));
+			return redirect("/");
+		}
+
+		if (User.confirm(user)) {
+			flash("success", Messages.get("Successfully validated"));
+			return redirect("/login");
+		} else {
+			flash("errorLink", Messages.get("error.confirm"));
+			return redirect("/");
+		}
+	}
+
+	public static Result validateNewslettter(String confirmationString) {
+		Newsletter newsletter = Newsletter
+				.findByConfirmationString(confirmationString);
+		if (confirmationString == null) {
+			flash("error", Messages.get("error"));
+			return redirect("/");
+		}
+		if (Newsletter.confirm(newsletter) == true) {
+			flash("successSubscribed", Messages.get("Successfully subscribed"));
+			return redirect("/");
+		} else {
+			flash("errorLink", Messages.get("error.confirm"));
+			return redirect("/");
+		}
+	}
+	
+	public static Result unsubscribe(String confirmationString) {
+		Newsletter newsletter = Newsletter
+				.findByConfirmationString(confirmationString);
+		if (newsletter == null) {
+			flash("error", Messages.get("error"));
+			return redirect("/");
+		}
+		if (Newsletter.unsubscribe(newsletter) == true) {
+			flash("SucessS", "Unsubsribed");
+			return redirect("/");
+		} else {
+			flash("errorUsub", "Error while unsubscribing");
+			return redirect("/");
+		}
+	}
 }
