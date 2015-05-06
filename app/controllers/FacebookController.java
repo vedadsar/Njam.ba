6c4b767f3c3963a05cb9600fcc21462c912782ef
Davor Stanković <dastko@gmail.com>
2015-05-06 11:53:31 +0200

    Added login via facebook

---
+++ app/controllers/FacebookController.java
@@ -0,0 +1,35 @@
+package controllers;
+
+import models.Location;
+import models.User;
+import play.Logger;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+public class FacebookController extends Controller {
+
+	public static Result sendFbJson() {
+
+		JsonNode data1 = request().body().asJson();
+		if (data1 == null) {
+			return badRequest("Expecting Json data");
+		}
+		String email = data1.findPath("email").textValue();
+		if (User.check(email) == true) {
+			User u = User.find(email);
+			u.locations.add(new Location(" ", " ", " "));
+			u.update();
+			session("email", email);
+			return ok("/user/" + email);
+		}
+		User user = new User(email, null);
+		user.validated = true;
+		user.save();
+
+		Logger.info("User email via facebook login:  " + email);
+		session("email", email);
+		return ok("/user/" + email);
+	}
+}
